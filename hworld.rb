e08c584fec4f179d9161f0e002f4212832364b15
Rob Dalton <rob-dalton@users.noreply.github.com>
2018-07-20 16:12:10 -0700

    Updated package version, sha256

--- hworld.rb
+++ hworld.rb
@@ -1,9 +1,9 @@
 class Hworld < Formula
   desc "Simple hello world script."
   homepage "https://github.com/rob-dalton/hworld"
-  url "https://github.com/rob-dalton/hworld/raw/master/hworld-0.1.tar.gz"
-  sha256 "d7923f3e6e41479b2f8a579c0d418eb29f24991b3636819114edfd6d954b778f"
-  version "0.1"
+  url "https://github.com/rob-dalton/hworld/raw/master/hworld-1.0.tar.gz"
+  sha256 "8443118e257c4c109332ae58df932da99f3bd1291a67b8a8a0283f529bc4f48e"
+  version "1.0"
 
   def install
     # install hworld script, create symlink to script in /usr/local/bin
